7c857a3b0db4bb2aa28481d95cad51c3c9271ae8
Gward2489 <gww8804@gmail.com>
2018-04-12 19:06:30 -0500

    working storing url string in database from django view

--- ticket_migrator/migrator_app/views/add_source_view.py
+++ ticket_migrator/migrator_app/views/add_source_view.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from migrator_app.models import *
+
+def renderAddSource(request):
+    return render(request, 'migrator_app/add_source.html')
 
 def addSource(request):
-    	"""
-		# This view allows a user to add a source repo they can import tickets from
-		# Author: Garrett Ward
-	    """
-    template = loader.get_template('migrator_app/add_source.html')
+    new_repo = source_repo_model.Source_Repo(url=request.POST['source_repo'])
+    new_repo.save()
+
+    return render(request, 'migrator_app/backlog_groomer.html')
     
+
         
