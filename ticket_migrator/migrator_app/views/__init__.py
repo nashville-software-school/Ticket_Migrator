0d4749e46536d3984e9fecdb0004c9cb480435c6
Christopher Miller <camiller.yr@gmail.com>
2018-04-16 13:10:27 -0500

    working- a bit?

--- ticket_migrator/migrator_app/views/__init__.py
+++ ticket_migrator/migrator_app/views/__init__.py
@@ -1,1 +1,1 @@
-from . import add_source_view, backlog_groomer_view, select_target_repos_view, success_view
+from . import add_source_view, backlog_groomer_view, select_target_repos_view, success_view, drag_view
