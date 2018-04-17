bf847aa0a9067fe7c62a7e8089d8a94aebb3974c
Christopher Miller <camiller.yr@gmail.com>
2018-04-17 10:58:39 -0500

    added ajax call to drag and drop

--- ticket_migrator/migrator_app/static/migrator_app/drag_drop.js
+++ ticket_migrator/migrator_app/static/migrator_app/drag_drop.js
@@ -1,13 +1,27 @@
-'use strict';
+"use strict";
 
-let dragula_obj = dragula([document.getElementById('dragBoxLeft'), document.getElementById('dragBoxRight')]);
+let dragula_obj = dragula([
+  document.getElementById("dragBoxLeft"),
+  document.getElementById("dragBoxRight")
+]);
 
-//query the children of the left container
-let results_container = $(dragula_obj.containers[0])[0].children
+$("#submit_button").click(e => {
+    e.preventDefault();
+    
+  let results_container = $(dragula_obj.containers[0])[0].children
+  let issue_array = Object.assign([], results_container).map((x, i) => { return { id: parseInt(x.id), priority: i }})
+  let backlog_name = $("#backlog_name").val()
+  let source_repo_id = $("#source_repo").val()
 
-dragula_obj.on('drop', e => {
-    $("#issue_array")[0].value = JSON.stringify(Object.assign([], results_container)
-        .map((x, i) => { return { 'id': parseInt(x.className), 'priority': i } }))
-})
+  let data = {"source_repo_id": source_repo_id, "backlog_name":backlog_name, "issue_array":issue_array}
 
+  $.ajax({
+    method: "POST",
+    url: "./create_backlog",
+    data: data
+  }).done(backlog_id => {
+    alert("Data Saved: " + msg);
+  });
+
+});
 //convert jQuery collection to array - map to convert results to a array of objects {id, priority}
