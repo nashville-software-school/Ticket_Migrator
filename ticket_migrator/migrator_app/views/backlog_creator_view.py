1f93600bd7bd832672b32439581eb6ac0085770c
Gward2489 <gww8804@gmail.com>
2018-04-17 10:00:53 -0500

    new view

---
+++ ticket_migrator/migrator_app/views/backlog_creator_view.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from migrator_app.models import *
+from django import forms
+
+def create_backlog_and_tickets(request)
+    
