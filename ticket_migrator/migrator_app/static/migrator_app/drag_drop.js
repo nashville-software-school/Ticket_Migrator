f24ba748e5bcbb678f52f182d976bb8ff1a3a6d8
Christopher Miller <camiller.yr@gmail.com>
2018-04-17 16:40:46 -0500

    integrated ajax and endpoint

--- ticket_migrator/migrator_app/static/migrator_app/drag_drop.js
+++ ticket_migrator/migrator_app/static/migrator_app/drag_drop.js
@@ -5,22 +5,56 @@ let dragula_obj = dragula([
   document.getElementById("dragBoxRight")
 ]);
 
+let saved_backlog = null
+let results_container = $(dragula_obj.containers[0])[0].children
+
+$("#home_button").click(e => {
+    window.location.href = "../"
+})
+
+$("#migrate_button").click(e => {
+    window.location.href = `../migrate/${saved_backlog}`
+})
+
+$("#create_button").click(e => {
+    $("#modal").hide()
+    $("#backlog_name")[0].value = ''
+    let r = $('.issue').remove()
+    $(dragula_obj.containers[1]).append(r)
+
+})
+
 $("#submit_button").click(e => {
     e.preventDefault();
     
-  let results_container = $(dragula_obj.containers[0])[0].children
-  let issue_array = Object.assign([], results_container).map((x, i) => { return { id: parseInt(x.id), priority: i }})
+  let issue_array = JSON.stringify(Object.assign([], results_container).map((x, i) => { return { id: parseInt(x.id), priority: i }}))
   let backlog_name = $("#backlog_name").val()
   let source_repo_id = $("#source_repo").val()
 
   let data = {"source_repo_id": source_repo_id, "backlog_name":backlog_name, "issue_array":issue_array}
 
+  function csrfSafeMethod(method) {
+    // these HTTP methods do not require CSRF protection
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+  }
+
+  let csrftoken = jQuery("[name=csrfmiddlewaretoken]").val();
+
+  $.ajaxSetup({
+    beforeSend: function(xhr, settings) {
+        if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+            xhr.setRequestHeader("X-CSRFToken", csrftoken);
+        }
+    }
+  });
+
   $.ajax({
     method: "POST",
-    url: "./create_backlog",
+    url: "../createbacklog/",
     data: data
-  }).done(backlog_id => {
-    alert("Data Saved: " + msg);
+  }).done(response => {
+    saved_backlog = response.backlog_id
+    $("#modal").show()
   });
 
 });
