a31758324df6c200f36b1f9ca18c9f0a5c51c31e
Christopher Miller <camiller.yr@gmail.com>
2018-04-18 12:29:00 -0500

    added redirect to adding backlog

--- ticket_migrator/migrator_app/static/migrator_app/drag_drop.js
+++ ticket_migrator/migrator_app/static/migrator_app/drag_drop.js
@@ -17,7 +17,7 @@ $("#migrate_button").click(e => {
 })
 
 $("#create_button").click(e => {
-    $("#modal").hide()
+    $(".modal").removeClass('is-active')
     $("#backlog_name")[0].value = ''
     let r = $('.issue').remove()
     $(dragula_obj.containers[1]).append(r)
@@ -54,7 +54,7 @@ $("#submit_button").click(e => {
     data: data
   }).done(response => {
     saved_backlog = response.backlog_id
-    $("#modal").show()
+    $(".modal").addclass('is-active')
   });
 
 });
