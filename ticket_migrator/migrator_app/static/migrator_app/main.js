3db71f77171245116ed73b2850cf5b2a8b0a5daa
John Dulaney <john.s.dulaney1@gmail.com>
2018-04-24 15:42:34 -0500

    :construction: added little check mark to show user is authenticated with a toke. Now about that actual logic lmao

--- ticket_migrator/migrator_app/static/migrator_app/main.js
+++ ticket_migrator/migrator_app/static/migrator_app/main.js
@@ -1,15 +1,19 @@
 //This function supposedly handles adding a token to local storage from the
 // input box in layout.html. Checks for any matches and nulls
-checkToken = () => {
-  // set the value of the fireld and the value of the localstorage, if any, into variables.
-  const tokenInputValue = document.getElementById('token_input_field').value;
-  const tokenAlreadyStored = JSON.parse(localStorage.getItem('Authorization'));
+$('#auth_false_show').show()
+$('#auth_true_show').hide()
 
-  if (tokenInputValue != tokenAlreadyStored && tokenInputValue != null) {
-      //throw it in
-    localStorage.setItem('Authorization', JSON.stringify(tokenInputValue));
-    //this willeventually hide/show the input field or whetever we use to show an authenticated user
-    $('#toke_new').toggleClass('is-active');
+checkToken = () => {
+    // set the value of the fireld and the value of the localstorage, if any, into variables.
+    const tokenInputValue = document.getElementById('token_input_field').value;
+    const tokenAlreadyStored = JSON.parse(localStorage.getItem('Authorization'));
+    
+    if (tokenInputValue != tokenAlreadyStored && tokenInputValue != null) {
+        //throw it in
+        localStorage.setItem('Authorization', JSON.stringify(tokenInputValue));
+        //this willeventually hide/show the input field or whetever we use to show an authenticated user
+        $('#auth_false_show').hide()
+        $('#auth_true_show').show()
     // c-log for developer info
     return console.log('token that went in:', tokenInputValue);
     //check if the token is equal to the one in localstorage, this is reduntant with the addition of hide/show logic
