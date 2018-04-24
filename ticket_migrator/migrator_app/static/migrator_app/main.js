485c1cb9a202f1d985f9a283f61c47d3344ba93a
John Dulaney <john.s.dulaney1@gmail.com>
2018-04-24 15:26:04 -0500

    :construction: added some intent via comments

--- ticket_migrator/migrator_app/static/migrator_app/main.js
+++ ticket_migrator/migrator_app/static/migrator_app/main.js
@@ -1,16 +1,24 @@
 //This function supposedly handles adding a token to local storage from the
 // input box in layout.html. Checks for any matches and nulls
 checkToken = () => {
-  const addToken = document.getElementById('token_input_field').value;
-  const getToken = JSON.parse(localStorage.getItem('Authorization'));
-debugger
-  if (addToken != getToken && addToken != null) {
-      localStorage.setItem("Authorization", JSON.stringify(addToken))
+  // set the value of the fireld and the value of the localstorage, if any, into variables.
+  const tokenInputValue = document.getElementById('token_input_field').value;
+  const tokenAlreadyStored = JSON.parse(localStorage.getItem('Authorization'));
+
+  if (tokenInputValue != tokenAlreadyStored && tokenInputValue != null) {
+      //throw it in
+    localStorage.setItem('Authorization', JSON.stringify(tokenInputValue));
+    //this willeventually hide/show the input field or whetever we use to show an authenticated user
     $('#toke_new').toggleClass('is-active');
-    return console.log('token went in');
-  } else if (addToken === getToken) {
-    return console.log('Sorry Dave, I already have that Token in my memory.');
-  } else if (addToken === null) {
-    return console.log('Gotta put somethin good in here');
-  }
+    // c-log for developer info
+    return console.log('token that went in:', tokenInputValue);
+    //check if the token is equal to the one in localstorage, this is reduntant with the addition of hide/show logic
+    } else if (tokenInputValue === tokenAlreadyStored) {
+        // c-log for developer info
+        return console.log('Sorry Dave, I already have that Token in my memory.');
+        //check to see if the input box is null, throw an error.
+    } else if (tokenInputValue === null) {
+        // c-log for developer info
+        return console.log('Gotta put somethin good in here');
+    }
 };
