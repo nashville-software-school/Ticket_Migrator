68104fde677ac2fee9e5c275a68842c8b5296859
Christopher Miller <camiller.yr@gmail.com>
2018-04-26 05:15:17 -0500

    added comments to javascript code

--- ticket_migrator/migrator_app/static/migrator_app/migrate.js
+++ ticket_migrator/migrator_app/static/migrator_app/migrate.js
@@ -1,3 +1,6 @@
+// Triggered on button click - adds another text input for target repos
+// Regex pattern checks that it starts with https:///github.com and then has something
+// that looks like camilleryr/bandstagram after it
 const addInput = () => {
   $("#target_repos_div").append(
     $(`
@@ -13,13 +16,16 @@ const addInput = () => {
   );
 };
 
+// Run on form submission to validate auth token and scrape values from input fields
 const set_form_values = () => {
+  //Check for presence of auth token and fail form submission if not present
   if (!get_auth_token()) {
     $("#ErrorHandler").toggleClass("is-active");
     $("#ErrorMessage").html("Please make sure you have saved a token to local storage");
     return false;
   }
 
+  //Check for some number of target repos and fail form submission if not present
   if (!get_target_repo_value()) {
     $("#ErrorHandler").toggleClass("is-active");
     $("#ErrorMessage").html("Please make sure you have entered in target repos");
@@ -27,6 +33,8 @@ const set_form_values = () => {
   }
 
   show_loading_gif_modal();
+
+  // Return true to trigger form submission
   return true;
 };
 
@@ -34,6 +42,10 @@ const show_loading_gif_modal = () => {
   $("#gifModal").toggleClass("is-active");
 };
 
+// Query all of the target repo input fields convert the html collection into an array
+// pull out the doman from the target repos entered discard the null values and set the results
+// to a form input so it can be submitted to the server
+// Return a bool value is equivelent to is there value?
 const get_target_repo_value = () => {
   return ($("#target_repos")[0].value = JSON.stringify(
     Object.assign([], $(".target_repos_inputs"))
@@ -44,6 +56,8 @@ const get_target_repo_value = () => {
     : false;
 };
 
+// Set the value of the "Authorization" key from local storage to a form value so it can
+// be submitted to the server - return a bool value that reflects if the token was present
 const get_auth_token = () => {
   return ($("#credentials")[0].value = JSON.parse(
     localStorage.getItem("Authorization")
@@ -52,6 +66,8 @@ const get_auth_token = () => {
     : false;
 };
 
+// Trigger the addInput function on button click
 $("#add").click(e => addInput());
 
-$(document).ready(addInput());  
+// Populate the dom with a single input on page load
+$(document).ready(addInput());
